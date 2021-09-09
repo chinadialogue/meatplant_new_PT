d8c1ef37c7ee9e1cb3858e11ffca27afbffdaec0
Julia <>
2021-09-09 17:54:03 +0200

    first commit

---
+++ data/data.js
@@ -0,0 +1,752 @@
+var data = [
+  {
+    "Company": "JBS S/A",
+    "City": "Barra Do Garças",
+    "State": "MT",
+    "SIF": 42,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -15.887167,
+    "Longitude": -52.243289
+  },
+  {
+    "Company": "PRODUTORES DE CARNE E DERIVADOS DE GURUPI",
+    "City": "Gurupi",
+    "State": "TO",
+    "SIF": 93,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -11.661871,
+    "Longitude": -49.03902
+  },
+  {
+    "Company": "FRIGORÍFICO RIO MARIA LTDA",
+    "City": "Rio Maria",
+    "State": "PA",
+    "SIF": 112,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -7.3031917,
+    "Longitude": -50.040604
+  },
+  {
+    "Company": "MINERVA S. A.",
+    "City": "Rolim De Moura",
+    "State": "RO",
+    "SIF": 791,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -11.72681,
+    "Longitude": -61.641278
+  },
+  {
+    "Company": "NATURAFRIG ALIMENTOS LTDA",
+    "City": "Barra Do Bugres",
+    "State": "MT",
+    "SIF": 1811,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -15.07372,
+    "Longitude": -57.235724
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Pontes E Lacerda",
+    "State": "MT",
+    "SIF": 1900,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -15.221274,
+    "Longitude": -59.324834
+  },
+  {
+    "Company": "MASTERBOI LTDA",
+    "City": "São Geraldo Do Araguaia",
+    "State": "PA",
+    "SIF": 2437,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -6.359418,
+    "Longitude": -48.623705
+  },
+  {
+    "Company": "FRIGOL S. A.",
+    "City": "Água Azul Do Norte",
+    "State": "PA",
+    "SIF": 2583,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -6.807094,
+    "Longitude": -50.519878
+  },
+  {
+    "Company": "PLENA ALIMENTOS S/A",
+    "City": "Paraíso Do Tocantins",
+    "State": "TO",
+    "SIF": 3215,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -10.248633,
+    "Longitude": -48.878485
+  },
+  {
+    "Company": "AGRA AGROINDUSTRIAL DE ALIMENTOS S/A",
+    "City": "Rondonópolis",
+    "State": "MT",
+    "SIF": 3941,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -16.538804,
+    "Longitude": -54.667137
+  },
+  {
+    "Company": "FRIGORIFICO REDENTOR S/A",
+    "City": "Guarantã Do Norte",
+    "State": "MT",
+    "SIF": 411,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -10.039325,
+    "Longitude": -54.914391
+  },
+  {
+    "Company": "VALE GRANDE INDUSTRIA E COMERCIO DE ALIMENTOS S/A",
+    "City": "Matupá",
+    "State": "MT",
+    "SIF": 4490,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -10.204934,
+    "Longitude": -54.970679
+  },
+  {
+    "Company": "MERCURIO ALIMENTOS S/A",
+    "City": "Castanhal",
+    "State": "PA",
+    "SIF": 4554,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -1.186565,
+    "Longitude": -47.947317
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Tangará da Serra",
+    "State": "MT",
+    "SIF": 1751,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -14.630349,
+    "Longitude": -57.551387
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Várzea Grande",
+    "State": "MT",
+    "SIF": 2015,
+    "Place": "Continental China/HK",
+    "Amazon": "Y",
+    "Latitude": -15.561286,
+    "Longitude": -56.086637
+  },
+  {
+    "Company": "FRIGORIFICO RIO MARIA LTDA",
+    "City": "Canaã Dos Carajás",
+    "State": "PA",
+    "SIF": 101,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -6.449465,
+    "Longitude": -49.839513
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Santana Do Araguaia",
+    "State": "PA",
+    "SIF": 1110,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -9.405617,
+    "Longitude": -50.423945
+  },
+  {
+    "Company": "PANTANEIRA INDÚSTRIA E COMÉRCIO DE CARNES E DERIVADOS LTDA",
+    "City": "Várzea Grande",
+    "State": "MT",
+    "SIF": 1206,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.584886,
+    "Longitude": -56.160528
+  },
+  {
+    "Company": "COMCARNE COMERCIAL DE CARNE LTDA",
+    "City": "Igarapé Do Meio",
+    "State": "MA",
+    "SIF": 1339,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -3.6879237,
+    "Longitude": -45.27826
+  },
+  {
+    "Company": "R E RIBEIRO SOARES & CIA LTDA",
+    "City": "Santarém",
+    "State": "PA",
+    "SIF": 1367,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -2.508494,
+    "Longitude": -54.655546
+  },
+  {
+    "Company": "FRIGORÍFICO VALE ALIMENTOS LTDA - ME",
+    "City": "Várzea Grande",
+    "State": "MT",
+    "SIF": 1409,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.571567,
+    "Longitude": -56.202211
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Tucumã",
+    "State": "PA",
+    "SIF": 1497,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -6.752958,
+    "Longitude": -51.114539
+  },
+  {
+    "Company": "INDUSTRIA E COMERCIO DE CARNES E DERIVADOS BOI BRASIL LTDA",
+    "City": "Alvorada",
+    "State": "TO",
+    "SIF": 1723,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -12.49368,
+    "Longitude": -49.138539
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "São Miguel Do Guaporé",
+    "State": "RO",
+    "SIF": 175,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.724528,
+    "Longitude": -62.699357
+  },
+  {
+    "Company": "FRIGOESTRELA S/A",
+    "City": "Rondonópolis",
+    "State": "MT",
+    "SIF": 1886,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -16.553747,
+    "Longitude": -54.677673
+  },
+  {
+    "Company": "FRIGORIFICO VALENCIO LTDA",
+    "City": "Xinguara",
+    "State": "PA",
+    "SIF": 1891,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.168202,
+    "Longitude": -49.97738
+  },
+  {
+    "Company": "MINERVA S. A.",
+    "City": "Araguaína",
+    "State": "TO",
+    "SIF": 1940,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.2802719,
+    "Longitude": -48.272082
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Juara",
+    "State": "MT",
+    "SIF": 200,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.28614,
+    "Longitude": -57.461452
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Pedra Preta",
+    "State": "MT",
+    "SIF": 2019,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -3.1085128,
+    "Longitude": -60.011655
+  },
+  {
+    "Company": "ABATEDOURO DE BOVINOS SAMPAIO LTDA - ME",
+    "City": "Redenção",
+    "State": "PA",
+    "SIF": 2258,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -8.034336,
+    "Longitude": -50.106377
+  },
+  {
+    "Company": "NEW BEEF COMPANY FRIGORIFICO S.A.",
+    "City": "Nova Xavantina",
+    "State": "MT",
+    "SIF": 2345,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -14.542228,
+    "Longitude": -52.367843
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Tucumã",
+    "State": "PA",
+    "SIF": 2350,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -6.7477376,
+    "Longitude": -51.126878
+  },
+  {
+    "Company": "RIO GRANDE COMÉRCIO DE CARNES LTDA",
+    "City": "Imperatriz",
+    "State": "MA",
+    "SIF": 2431,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -5.461215,
+    "Longitude": -47.473361
+  },
+  {
+    "Company": "IRMÃOS GONÇALVES COMÉRCIO E INDÚSTRIA LTDA",
+    "City": "Jaru",
+    "State": "RO",
+    "SIF": 2443,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.472465,
+    "Longitude": -62.447467
+  },
+  {
+    "Company": "MINERVA S.A.",
+    "City": "Paranatinga",
+    "State": "MT",
+    "SIF": 2500,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -14.463916,
+    "Longitude": -54.04053
+  },
+  {
+    "Company": "FRIGO 10 LTDA",
+    "City": "Boa Vista",
+    "State": "RR",
+    "SIF": 2515,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": 2.7091999,
+    "Longitude": -60.824131
+  },
+  {
+    "Company": "GOLDEN IMEX EIRELI",
+    "City": "Juruena",
+    "State": "MT",
+    "SIF": 2620,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.3525,
+    "Longitude": -58.508403
+  },
+  {
+    "Company": "ATIVO ALIMENTOS EXPORTADORA E IMPORTADORA EIRELI",
+    "City": "Castanhal",
+    "State": "PA",
+    "SIF": 2801,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -1.3243878,
+    "Longitude": -47.914097
+  },
+  {
+    "Company": "AGROPAM - AGRICULTURA E PECUARIA AMAZONAS S/A",
+    "City": "Boca do Acre",
+    "State": "AM",
+    "SIF": 2803,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -8.7873032,
+    "Longitude": -67.320142
+  },
+  {
+    "Company": "INDUSTRIA E COMERCIO DE CARNES E DERIVADOS BOI BRASIL LTDA",
+    "City": "Araguaína",
+    "State": "TO",
+    "SIF": 2852,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.1568345,
+    "Longitude": -48.229465
+  },
+  {
+    "Company": "JBS S. A.",
+    "City": "Pimenta Bueno",
+    "State": "RO",
+    "SIF": 2880,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.604527,
+    "Longitude": -61.226775
+  },
+  {
+    "Company": "MINERVA S. A.",
+    "City": "Mirassol D'Oeste",
+    "State": "MT",
+    "SIF": 2911,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.691314,
+    "Longitude": -58.126004
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S.A.",
+    "City": "Várzea Grande",
+    "State": "MT",
+    "SIF": 292,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.630481,
+    "Longitude": -56.09188
+  },
+  {
+    "Company": "R C MOREIRA COSTA",
+    "City": "Rondon Do Pará",
+    "State": "PA",
+    "SIF": 2927,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -4.7893249,
+    "Longitude": -48.111197
+  },
+  {
+    "Company": "VALE GRANDE INDUSTRIA E COMERCIO DE ALIMENTOS S/A",
+    "City": "Nova Canaã Do Norte",
+    "State": "MT",
+    "SIF": 2937,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.61559,
+    "Longitude": -55.685705
+  },
+  {
+    "Company": "JBS S. A.",
+    "City": "Juína",
+    "State": "MT",
+    "SIF": 2942,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.458308,
+    "Longitude": -58.79691
+  },
+  {
+    "Company": "JBS S. A.",
+    "City": "Rio Branco",
+    "State": "AC",
+    "SIF": 2951,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.010765,
+    "Longitude": -67.743903
+  },
+  {
+    "Company": "JBS S. A.",
+    "City": "Araputanga",
+    "State": "MT",
+    "SIF": 2979,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.484995,
+    "Longitude": -58.349155
+  },
+  {
+    "Company": "JBS S. A.",
+    "City": "Diamantino",
+    "State": "MT",
+    "SIF": 3000,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -14.35812,
+    "Longitude": -56.356584
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Chupinguaia",
+    "State": "RO",
+    "SIF": 3250,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -12.579889,
+    "Longitude": -60.908829
+  },
+  {
+    "Company": "FRISACRE FRIGORIFICO SANTO AFONSO DO ACRE LTDA - EPP",
+    "City": "Rio Branco",
+    "State": "AC",
+    "SIF": 3297,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.010533,
+    "Longitude": -67.741157
+  },
+  {
+    "Company": "VALE GRANDE INDUSTRIA E COMERCIO DE ALIMENTOS S/A",
+    "City": "Sinop",
+    "State": "MT",
+    "SIF": 3348,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.951287,
+    "Longitude": -55.519683
+  },
+  {
+    "Company": "MARFRIG GLOBAL FOODS S. A.",
+    "City": "Ji-Paraná",
+    "State": "RO",
+    "SIF": 3405,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.935834,
+    "Longitude": -61.971479
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Confresa",
+    "State": "MT",
+    "SIF": 3470,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.611544,
+    "Longitude": -51.633338
+  },
+  {
+    "Company": "FRIGORIFICO FORTEFRIGO LTDA",
+    "City": "Paragominas",
+    "State": "PA",
+    "SIF": 372,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -2.949943,
+    "Longitude": -47.386737
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Araguaína",
+    "State": "TO",
+    "SIF": 4001,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.2805529,
+    "Longitude": -48.250157
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Água Boa",
+    "State": "MT",
+    "SIF": 4121,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -14.021011,
+    "Longitude": -52.184161
+  },
+  {
+    "Company": "FRIGOL S. A.",
+    "City": "São Félix do Xingu",
+    "State": "PA",
+    "SIF": 4150,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -6.6688437,
+    "Longitude": -51.954594
+  },
+  {
+    "Company": "FRIGORIFICO RIO MACHADO INDUSTRIA E COMERCIO DE CARNES LTDA",
+    "City": "Ji-Paraná",
+    "State": "RO",
+    "SIF": 4267,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.901122,
+    "Longitude": -61.885763
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Colíder",
+    "State": "MT",
+    "SIF": 4268,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.788094,
+    "Longitude": -55.459277
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Alta Floresta",
+    "State": "MT",
+    "SIF": 4302,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -9.88026,
+    "Longitude": -56.153108
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Vilhena",
+    "State": "RO",
+    "SIF": 4333,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -12.731053,
+    "Longitude": -60.169552
+  },
+  {
+    "Company": "DISTRIBOI - INDUSTRIA COMÉRCIO E TRANSPORTE DE CARNE BOVINA LTDA",
+    "City": "Rolim De Moura",
+    "State": "RO",
+    "SIF": 4334,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -11.726017,
+    "Longitude": -61.725035
+  },
+  {
+    "Company": "FTS - FRIGORIFICO TAVARES DA SILVA LTDA",
+    "City": "Xinguara",
+    "State": "PA",
+    "SIF": 4398,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.1275508,
+    "Longitude": -49.940037
+  },
+  {
+    "Company": "MERCURIO ALIMENTOS S/A",
+    "City": "Xinguara",
+    "State": "PA",
+    "SIF": 4413,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.1563805,
+    "Longitude": -49.962006
+  },
+  {
+    "Company": "INTERMainland FOODS - COMERCIO DE ALIMENTOS LTDA - EPP",
+    "City": "Rondonópolis",
+    "State": "MT",
+    "SIF": 4466,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -16.509989,
+    "Longitude": -54.618458
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Marabá",
+    "State": "PA",
+    "SIF": 457,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -5.3869966,
+    "Longitude": -49.071543
+  },
+  {
+    "Company": "FRIGORIFICO PARAISO EIRELI",
+    "City": "Paraíso Do Tocantins",
+    "State": "TO",
+    "SIF": 4625,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.224244,
+    "Longitude": -48.928119
+  },
+  {
+    "Company": "163 BEEF INDÚSTRIA E COMÉRCIO DE CARNES LTDA",
+    "City": "Novo Progresso",
+    "State": "PA",
+    "SIF": 4686,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -6.8520915,
+    "Longitude": -55.463244
+  },
+  {
+    "Company": "DISTRIBOI - INDUSTRIA COMÉRCIO E TRANSPORTE DE CARNE BOVINA LTDA",
+    "City": "Ji-Paraná",
+    "State": "RO",
+    "SIF": 4695,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -10.883668,
+    "Longitude": -61.878284
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Pontes E Lacerda",
+    "State": "MT",
+    "SIF": 51,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.172873,
+    "Longitude": -59.322857
+  },
+  {
+    "Company": "FRIGORÍFICO PANTANAL LTDA",
+    "City": "Várzea Grande",
+    "State": "MT",
+    "SIF": 585,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -15.723186,
+    "Longitude": -56.160112
+  },
+  {
+    "Company": "L K J - FRIGORIFICO LTDA",
+    "City": "Araguaína",
+    "State": "TO",
+    "SIF": 723,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.2002029,
+    "Longitude": -48.253001
+  },
+  {
+    "Company": "JBS S/A",
+    "City": "Redenção",
+    "State": "PA",
+    "SIF": 807,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -8.0579255,
+    "Longitude": -50.024701
+  },
+  {
+    "Company": "MASTERBOI LTDA",
+    "City": "Nova Olinda",
+    "State": "TO",
+    "SIF": 860,
+    "Place": "HK",
+    "Amazon": "Y",
+    "Latitude": -7.6209102,
+    "Longitude": -48.422807
+  }
+]
